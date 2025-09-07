d84864e849863ed8ad51fe63b1dfae0b16d85d37
technologi-service <advancedinthefuture@gmail.com>
2025-09-07 08:27:35 +0200

    vistas init creada

---
+++ src/actions/clientes/get-clientes.ts
@@ -0,0 +1,17 @@
+import { defineAction } from 'astro:actions';
+import prisma from '../../db/prisma';
+
+export const getAllClientes = defineAction({
+  accept: 'json',
+  handler: async ({ request }) => {
+    try {
+      const clientes = await prisma.clientes.findMany({
+        take: 100, // Limitar a 100 clientes para evitar sobrecarga
+      });
+      return clientes;
+    } catch (error) {
+      console.error('Error fetching clientes:', error);
+      throw new Error('Failed to fetch clientes');
+    }
+  },
+});
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
+import { getAllClientes } from "./clientes/get-clientes";
 import { getRangos } from "./config-rangos/get-rangos";
 import { getVariables } from "./config-variables.ts/get-variables";
 import { updateVariable } from "./config-variables.ts/update-variables";
@@ -6,5 +7,6 @@ export const server = {
   getVariables,
   updateVariable,
   getRangos,
+  getAllClientes,
 
 }
