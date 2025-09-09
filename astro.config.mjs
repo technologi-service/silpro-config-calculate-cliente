7d394080a65483a76e60b6b20fc1c54b496bed1c
technologi-service <advancedinthefuture@gmail.com>
2025-09-09 11:45:52 +0200

    render configurado 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 
-
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,7 +17,12 @@ export default defineConfig({
 
   output: "server",
 
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone'
+  }),
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+  },
 
   integrations: [react()]
 });
